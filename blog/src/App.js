dd449fd6cc6dcc6ed93d5f6bf4df0fdef27e235e
KingGaemi <geami1054@gmail.com>
2024-05-08 14:34:59 +0900

    New React Project

---
+++ blog/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My First React App</h1>
+      <div className="black-nav">
+        
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
